f7fcb5dec4a543a9d4fa15ab24b335216d7398b8
Peter Williams <peter.williams@papercut.com>
2019-01-01 08:42:03 +1100

    Added test runners for GhostScript and PDFBox.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Install [SpaCy](https://spacy.io/usage/)
 ### UniDoc
 Build [pdf_info](https://github.com/peterwilliams97/unidoc-examples/blob/benchmark/pdf/analysis/pdf_info.go) and [pdf_extract_text](https://github.com/peterwilliams97/unidoc-examples/blob/benchmark/pdf/text/pdf_extract_text.go)
 
-(This branch has test programs modified to work with this script.)
+(The above UniDoc [branch](https://github.com/peterwilliams97/unidoc-examples/blob/benchmark) has test programs modified to work with this script.
 
 	pushd $GOPATH/src/github.com/unidoc/unidoc-examples/pdf/text/
 	go build pdf_to_text.go
@@ -56,9 +56,8 @@ A line from [results.txt](results.txt) is
 	801 0.085 0.114 0.148: 01767-0240c6.txt [1.3] 0.712 MB 2 pages ~/testdata/Wild_comes_the_molten_ore.pdf
 
 * This is the 802nd (801 with zero-offset) successful comparison.
-* It was run on `~/testdata/Wild_comes_the_molten_ore.pdf` which ois `PDF version 1.3`, `0.712 MBytes` and has `2 pages`.
-* The [Jaccard distance](https://en.wikipedia.org/wiki/Jaccard_index) between the UniDoc and Poppler text extractions are `0.085`, `0.114` and `0.148` for `unigrams`, `bigrams` and `trigrams` respectively, which is a fairly good, but not great match.
+* It was run on `~/testdata/Wild_comes_the_molten_ore.pdf` which is `PDF version 1.3`, `0.712 MBytes` and has `2 pages`.
+* The [Jaccard distance](https://en.wikipedia.org/wiki/Jaccard_index) between the UniDoc and Poppler text extractions are `0.085`, `0.114` and `0.148` for `unigrams`, `bigrams` and `trigrams` respectively which is a fairly good, but not great, match.
 
 ## Using Results
 If you can assume that one of the PDF text extractors works well then you go to the PDF files that give the most different text extraction, the ones at the bottom of the results, and work on them.
-
--- benchmark.py
+++ benchmark.py
@@ -14,68 +14,176 @@ from subprocess import call, Popen, PIPE
 import spacy
 
 
+SHA1_LEN = 10         # We use the first 10 hex characters of PDF files SHA-1 hash for indexing.
 MB = 1024.0 * 1024.0
 TMP = 'temp.txt'
 
 
 def read(path):
+    "read returns the contents of text file `path`"
     with open(path, 'rt') as f:
         return f.read()
 
 
+def write(path, text):
+    "write writes the string `text` to file `path`"
+    with open(path, 'wt') as f:
+        f.write(text)
+
+
 def file_sha1(path):
+    "file_sha1 returns the first 10 hex characters of the SHA-1 hash of file `path`."
     with open(path, 'rb') as f:
         data = f.read()
-    return hashlib.sha1(data).hexdigest()
+    sha1 = hashlib.sha1(data).hexdigest()
+    return sha1[:SHA1_LEN].rjust(SHA1_LEN, '0')
 
 
-RE_VERSION = re.compile(r'PDF Version:\s*(\d+\.\d+)?')
-RE_PAGES = re.compile(r'Num Pages:\s*(\d+)')
-RE_ENCRYPTED = re.compile(r'Is Encrypted:\s*(true|false)')
-RE_VIEWABLE = re.compile(r'Is Viewable \(without pass\):\s*(true|false)')
+RE_PATH = re.compile('Input file:\\s*(.+?)\\s*$')
+RE_VERSION = re.compile('PDF Version:\\s*(\\d+\\.\\d+)?')
+RE_PAGES = re.compile('Num Pages:\\s*(\\d+)$')
+RE_ENCRYPTED = re.compile('Is Encrypted:\\s*(true|false)')
+RE_VIEWABLE = re.compile('Is Viewable \\(without pass\\):\\s*(true|false)')
 
 
-def get_val(regex, output):
-    m = regex.search(str(output))
-    assert m, (regex.pattern, output)
-    val = m.group(1)
-    # assert val is not None
-    # assert val != 'None'
-    return val
+def get_val(regex, lines):
+    llines = lines.split('\n')
+    for line in llines:
+        m = regex.search(line)
+        if m:
+            return m.group(1)
+    return None
 
 
-def pdf_info(path):
+def parse_info(output):
     """
         PDF Version: 1.5
         Num Pages: 248
         Is Encrypted: false
         Is Viewable (without pass): true
     """
-    process = Popen(['./pdf_info', path], stdout=PIPE)
+    path = get_val(RE_PATH, output)
+    info = {
+        'path': path,
+        'size': getsize(path) / MB
+    }
+    version = get_val(RE_VERSION, output)
+    if version is not None:
+        info['version'] = version
+    pages = get_val(RE_PAGES, output)
+    if pages is not None:
+        info['pages'] = int(pages)
+    encrypted = get_val(RE_ENCRYPTED, output)
+    if encrypted is not None:
+        info['encrypted'] = encrypted == 'true'
+    viewable = get_val(RE_VIEWABLE, output)
+    if viewable is not None:
+        info['viewable'] = viewable == 'true'
+    return info
+
+
+def pdf_info(path):
+    process = Popen(['./pdf_info', path], stdout=PIPE, universal_newlines=True)
+    output, err = process.communicate()
+    exit_code = process.wait()
+    if exit_code != 0:
+        return {'path': path, 'size': getsize(path)}
+    info = parse_info(output)
+    assert info['path'] == path, (path, info, output)
+    return info
+
+
+def read_info(info_path):
+    with open(info_path, 'rt') as f:
+        output = f.read()
+    # output = read(info_path)
+    return parse_info(output)
+
+
+def write_info(info_path, info):
+    parts = [
+        'Input file: %s\n' % info['path'],
+        'Size: %.3f MB\n' % info['size'],
+    ]
+    if 'version' in info:
+        parts.extend([
+        'PDF Version: %s\n' % info['version'],
+        'Num Pages: %d\n' % info['pages'],
+        'Is Encrypted: %s\n' % str(info['encrypted']).lower(),
+        'Is Viewable (without pass): %s\n' % str(info['viewable']).lower(),
+    ])
+    write(info_path, ''.join(parts))
+
+
+def info_str(info, with_path=True):
+    if not info_good(info):
+        msg ='INVALID %5.3f MB' % info['size']
+    else:
+        version = info['version']
+        if version is None:
+            version = '---'
+        msg = '[%s] %5.3f MB %3d pages' % (version, info['size'], info['pages'])
+    if with_path:
+        msg = '%s %s' % (msg, info['path'])
+    return msg
+
+
+def info_good(info):
+    if not info:
+        return False
+    return all((key in info) for key in ['version', 'pages'])
+
+
+RE_SECTION = re.compile(r'========= Subtype\s+(.+?)\s+[=]+ Subtype', re.DOTALL)
+RE_SUBTYPE = re.compile(r'\d+: (Type\d+\S*)\s+\d+\s+occurrences', re.DOTALL | re.MULTILINE)
+
+
+def parse_fonts(text):
+    """
+        ===================================================== Subtype
+        All versions
+        1 files tested
+        15 font subtype occurrences
+        2 subtypes
+         0: Type0:CIDFontType0    2 occurrences (13%) Occurred in   1 (100%) of files.
+         1: Type1                13 occurrences (87%) Occurred in   1 (100%) of files.
+        ===================================================== Subtype
+    """
+    m = RE_SECTION.search(text)
+    if not m: return {}
+    assert m, text
+    section = m.group(1)
+    subtypes = defaultdict(int)
+    for m in RE_SUBTYPE.finditer(section):
+        subtypes[m.group(1)] += 1
+    return subtypes
+
+
+def pdf_fonts(path):
+    process = Popen(['./pdf_fonts', path], stdout=PIPE, universal_newlines=True)
     output, err = process.communicate()
     exit_code = process.wait()
     if exit_code != 0:
         return {}
-    return {
-        'path': path,
-        'size': getsize(path) / MB,
-        'version': get_val(RE_VERSION, output),
-        'pages': int(get_val(RE_PAGES, output)),
-        'encrypted': get_val(RE_ENCRYPTED, output) == 'true',
-        'viewable': get_val(RE_VIEWABLE, output) == 'true',
-    }
+    try:
+        fonts = parse_fonts(output)
+    except AssertionError:
+        print('>>>>>>>>path=%s' % path, file=sys.stderr)
+        raise
+    return {k: v for k, v in fonts.items()}
 
 
-def info_str(info, with_path=True):
-    if not info:
-        return 'INVALID'
-    version = info['version']
-    if version is None:
-        version = '---'
-    msg = '[%s] %5.3f MB %3d pages' % (version, info['size'], info['pages'])
-    if with_path:
-        msg = '%s %s' % (msg, info['path'])
-    return msg
+def read_fonts(font_path):
+    return parse_fonts(read(font_path))
+
+
+def write_fonts(font_path, fonts):
+    parts = []
+    parts.append('===================================================== Subtype\n')
+    for i, (font, count) in enumerate(sorted(fonts.items())):
+        parts.append('%d: %s %d occurrences \n' % (i, font, count))
+    parts.append('===================================================== Subtype\n')
+    write(font_path, ''.join(parts))
 
 
 def run_mutool(path, tmp_path):
@@ -86,10 +194,19 @@ def run_pstopdf(path, tmp_path):
     call(['pdftotext', '-enc', 'UTF-8', path, tmp_path])
 
 
-def run_unidoc(path, tmp_path):
-    print('run_unidoc:', ['./pdf_to_text', path, tmp_path])
-    call(['./pdf_to_text', path, tmp_path])
+PDFBOX_app = '~/pdf/pdfbox.orig/app/target/pdfbox-app-2.0.9-SNAPSHOT.jar'
+PDFBOX_app = os.path.expanduser(PDFBOX_app)
 
+
+def run_pdfbox(path, tmp_path):
+    process = Popen(['java', '-jar', PDFBOX_app, 'ExtractText', '-sort', path, tmp_path],
+        stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    output, err = process.communicate()
+    exit_code = process.wait()
+
+
+def run_unidoc(path, tmp_path):
+    call(['./pdf_to_text', path, tmp_path])
 
 
 def exec_runner(path, runner, tmp_path):
@@ -113,7 +230,7 @@ nlp = spacy.load('en_core_web_sm')
 
 
 def tokenize(data):
-    """tokenize returns the words in byte array `data`"""
+    """tokenize returns the words in byte array `data`."""
     s = str(data)
     s = RE_SPACE.sub(' ', s)
     doc = nlp(s)
@@ -195,6 +312,8 @@ def get_test_files(argv, max_size_mb=None, max_files=None):
 
 
 def get_sha1_info(path_list, min_version='1.3', max_pages=100):
+    results_dir = os.path.join('results', 'info')
+    os.makedirs(results_dir, exist_ok=True)
     path_sha1 = {}
     sha1_info = {}
     for i, path in enumerate(path_list):
@@ -204,13 +323,31 @@ def get_sha1_info(path_list, min_version='1.3', max_pages=100):
             print('**! DUPLICATE (%d of %d) %s (%s)' % (i + 1, len(path_list), path,
                 sha1_info[sha1]), flush=True)
             continue
-        info = pdf_info(path)
-        if not info or info['encrypted'] or not info['viewable'] or info['version'] < min_version:
+        info_path = os.path.join(results_dir, '%s.info' % sha1)
+        if os.path.exists(info_path):
+            info = read_info(info_path)
+        else:
+            info = pdf_info(path)
+            write_info(info_path, info)
+
+        if not info_good(info) or info['encrypted'] or not info['viewable'] or info['version'] < min_version:
             print('**- %s (%d of %d) %s' % (info_str(info, False), i + 1, len(path_list), path), flush=True)
             continue
         if info['pages'] > max_pages:
             print('**+ %s (%d of %d) %s' % (info_str(info, False), i + 1, len(path_list), path), flush=True)
             continue
+
+        font_path = os.path.join(results_dir, '%s.fonts' % sha1)
+        if os.path.exists(font_path) and False:
+            font = read_fonts(font_path)
+        else:
+            fonts = pdf_fonts(path)
+            write_fonts(font_path, fonts)
+
+        if 'Type3' in fonts or 'Type0:CIDFontType0' in fonts:
+            print('**x %s (%d of %d) %s' % (fonts, i + 1, len(path_list), path), flush=True)
+            continue
+
         path_sha1[path] = sha1
         sha1_info[sha1] = info
 
@@ -221,6 +358,7 @@ def get_sha1_info(path_list, min_version='1.3', max_pages=100):
 TESTS = {
     # 'ghostscript': run_mutool,
     'poppler': run_pstopdf,
+    # 'pdfbox': run_pdfbox,
     'unidoc': run_unidoc,
 }
 
@@ -231,7 +369,7 @@ def main():
     path_list, path_sha1, sha1_info = get_sha1_info(path_list)
     print('get_sha1_info: %d files' % len(path_list))
 
-    # Run the runners on all the files in path_list.
+    # Run the runners on all the files in `path_list`.
     for test in sorted(TESTS):
         runner = TESTS[test]
         tmp_path = os.path.join('results', 'tmp.%s.txt' % test)
@@ -247,6 +385,8 @@ def main():
             if not run_test(path, runner, tmp_path, dest):
                 print('%s failed. %s' % (test, info_str(info)), flush=True)
 
+    # Reader the runners' extracted texts.
+    print('+' * 80, flush=True)
     sha1_successes = defaultdict(list)
     for i, path in enumerate(path_list):
         sha1 = path_sha1[path]
@@ -269,7 +409,8 @@ def main():
         # if len(sha1_successes) >= 20:
         #     break
 
-    print('#' * 80)
+    # Compute differences between the results.
+    print('#' * 80, flush=True)
     scores = {}
     name_sha1 = {}
     for sha1, paths in sorted(sha1_successes.items()):
@@ -289,7 +430,7 @@ def main():
         print("name=%s jac=%.2f %.2f %.2f n=%d %d %s" % (
             name, jac[0], jac[1], jac[2], len(words1), len(words2), info_str(info)))
         print("1) %d %s" % (len(diff1), diff1[:10]))
-        print("2) %d %s" % (len(diff2), diff2[:10]))
+        print("2) %d %s" % (len(diff2), diff2[:10]), flush=True)
 
     print("&" * 80)
     for i, name in enumerate(sorted(scores, key=lambda n: (scores[n][0], scores[n][1], scores[n][2], n))):
